2a45e6bb797a079439ef321b5afdd42cc45b0d71
Josep Maria Cagigal Soler <josemariacagigalso@gmail.com>
2016-04-08 17:46:44 +0200

    added favicon

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'})); //parse application/vnd.api+json as json
